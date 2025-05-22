a13ffdb97ec67b648443e24c9b57970d2dd42dfa
ban. <knzvanz1@gmail.com>
2025-05-22 14:15:30 +0800

    [Customer Portal][Staging Env] - Sender Address Test (Valid, Empty)

---
+++ constants/customer/TEST_CUSTOMER.ts
@@ -0,0 +1,8 @@
+const TEST_CASE_CUSTOMER = {
+  "001": "Valid Sender Address",
+  "002": "Invalid Sender Address",
+  "003": "Empty Sender Address",
+  "004": "Invalid Sender Address",
+};
+
+export default TEST_CASE_CUSTOMER;
